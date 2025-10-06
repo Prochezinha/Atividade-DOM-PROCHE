34dc593badd0e8441d1eb847cb19446b30f58ffe
Prochezinha <nicolly.proche.nascimento@escola.pr.gov.br>
2025-10-06 13:58:24 +0000

    salvar

---
+++ Script.js
@@ -0,0 +1,29 @@
+// Seletores do DOM
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementsById('taskInput');
+const taskList = document.getElementById('taskList');
+
+// Função que cria um <li> com texto e botão "Remover"
+function createTaskItem(text) {
+const li = document.createElement('li');
+const span = document.createElement('span');
+span.textContent = text;
+const deleteBtn = document.createElement('button');
+deleteBtn.textContent = 'Remover';
+deleteBtn.className = 'delete-btn';
+deleteBtn.type = 'button';
+li.appendChild(span);
+li.appendChild(deleteBnt);
+return li;
+}
+
+// Captura do Submit 
+taskForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const text = taskInput.value.trim();
+  if (!text) return;
+  const li = createTaskItem(text);
+  taskList.appendChild(li);
+  taskInput.value = '';
+  taskInput.focus();
+});
